cd7f98fc95713678970c3aa092042ee2a18168d6
moonshot-aayushi <51299602+moonshot-aayushi@users.noreply.github.com>
2020-03-31 14:55:26 +0530

    Add files via upload

---
+++ color.js
@@ -0,0 +1,36 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+
+var body = document.getElementById("gradient");
+
+
+// body.style.background = "red"
+
+// var c1;
+// var c2;
+
+// var body = document.getElementById()
+// function color1(){
+// 	c1 = color1.value;
+// }
+// function color2(){
+// 	c2 = color2.value;
+// }
+
+function colorPick(){
+body.style.background = 
+	"linear-gradient(to right," 
+	+ color1.value +
+	 "," +
+	  color2.value 
+	  +")";
+
+	  css.textContent = body.style.background + ";";
+}
+
+
+
+// color1.addEventListener("input", colorPick);
+
+// color2.addEventListener("input", colorPick);
